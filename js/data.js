96794d793599f79b9e3b1c00713741ae3fef417a
Balder <2290512627@qq.com>
2022-05-24 12:43:32 +0800

    网站初部署

---
+++ js/data.js
@@ -0,0 +1,308 @@
+// "bvedio", "bstudy", "bentertainment", "bcode", "bitem"
+var data = {
+  bvedioData: [
+    {
+      title: "爱奇艺",
+      pic: "爱奇艺.webp",
+      href: "https://www.iqiyi.com/",
+    },
+    {
+        title: "芒果TV",
+        pic: "芒果TV.jfif",
+        href: "https://www.mgtv.com/",
+      },
+      {
+        title: "腾讯视频",
+        pic: "腾讯视频.png",
+        href: "https://v.qq.com/",
+      },
+      {
+        title: "茶杯狐",
+        pic: "茶杯狐.jfif",
+        href: "https://www.cupfox.com/",
+      },
+      {
+        title: "哔哩哔哩bilibili",
+        pic: "bilibili.webp",
+        href: "https://www.bilibili.com/",
+      },
+      {
+        title: "抖音",
+        pic: "抖音.webp",
+        href:"https://www.douyin.com/"
+      }
+  ],
+  bstudyData: [
+    {
+      title: "腾讯课堂",
+      pic: "腾讯课堂.png",
+      href: "https://ke.qq.com/",
+    },
+    {
+        title: "课堂派",
+        pic: "课堂派.png",
+        href: "https://www.ketangpai.com/",
+      },
+      {
+        title: "语雀",
+        pic: "语雀.png",
+        href: "https://www.yuque.com/",
+      },
+      {
+        title: "阿里云",
+        pic: "阿里云.png",
+        href: "https://www.aliyun.com/",
+      },
+      {
+        title: "中国大学MOOC",
+        pic: "中国大学mooc.png",
+        href: "https://www.icourse163.org/",
+      },
+      {
+        title: "有道云笔记",
+        pic: "有道云.png",
+        href: "https://note.youdao.com/",
+      },
+      {
+        title: "百度翻译",
+        pic: "百度翻译.png",
+        href: "fanyi.baidu.com/",
+      },
+      {
+        title: "百度脑图",
+        pic: "爱奇艺.webp",
+        href: "https://naotu.baidu.com",
+      },
+      {
+        title: "ProccessOn",
+        pic: "ProcessOn.png",
+        href: "https://www.processon.com",
+      },
+      {
+        title: "阿里云盘",
+        pic: "阿里云盘.png",
+        href: "https://www.aliyundrive.com/drive",
+      },
+      {
+        title: "qq邮箱",
+        pic: "qq邮箱.png",
+        href: "https://mail.qq.com/",
+      },
+      {
+        title: "百度网盘",
+        pic: "腾讯课堂.png",
+        href: "https://pan.baidu.com/",
+      },
+      {
+        title: "改图宝",
+        pic: "腾讯课堂.png",
+        href: "https://www.gaitubao.com/",
+      },
+      {
+        title: "ico生成",
+        pic: "腾讯课堂.png",
+        href: "https://www.bitbug.net/",
+      },
+      {
+        title: "问卷星",
+        pic: "问卷星.png",
+        href: "https://www.wjx.cn/",
+      },
+      {
+        title: "智慧树",
+        pic: "爱奇艺.webp",
+        href: "https://www.zhihuishu.com/",
+      },
+      {
+        title: "PTA",
+        pic: "pta.png",
+        href: "https://pintia.cn/",
+      },
+  ],
+  bentertainmentData: [
+    {
+      title: "网易云音乐",
+      pic: "网易云.png",
+      href: "https://music.163.com/",
+    },
+    {
+        title: "qq音乐",
+        pic: "qq音乐.png",
+        href: "https://y.qq.com/",
+      },
+      {
+        title: "酷狗音乐",
+        pic: "酷狗音乐.png",
+        href: "https://www.kugou.com/",
+      },
+      {
+        title: "小米官网",
+        pic: "小米官网.png",
+        href: "https://www.mi.com/",
+      },
+      {
+        title: "淘宝",
+        pic: "淘宝.png",
+        href: "https://www.taobao.com/",
+      },
+      {
+        title: "京东",
+        pic: "京东.png",
+        href: "https://www.jd.com/",
+      },
+      {
+        title: "摹客",
+        pic: "摹客.png",
+        href: "https://www.mockplus.cn/",
+      },
+      {
+        title: "Pixco手机界面",
+        pic: "pixco.png",
+        href: "https://pixso.design/",
+      },
+      {
+        title: "新拟态设计",
+        pic: "爱奇艺.webp",
+        href: "https://neumorphism.io/",
+      },
+      {
+        title: "斗鱼",
+        pic: "斗鱼.png",
+        href: "https://www.douyu.com/",
+      },
+      {
+        title: "虎牙",
+        pic: "虎牙.png",
+        href: "https://www.huya.com/",
+      },
+      {
+        title: "键盘测试",
+        pic: "键盘测试.png",
+        href: "https://keyboard.bmcx.com/",
+      },
+      {
+        title: "typing",
+        pic: "typing.png",
+        href: "https://barneyzhao.gitee.io/typing-cn/",
+      },
+  ],
+  bcodeData: [
+    {
+      title: "Gitee",
+      pic: "gitee.png",
+      href: "https://gitee.com/",
+    },
+    {
+      title: "github",
+      pic: "github.png",
+      href: "https://github.com/",
+    },
+    {
+        title: "CSDN",
+        pic: "csdn.jfif",
+        href: "https://blog.csdn.net/",
+      },
+      {
+        title: "博客园",
+        pic: "爱奇艺.webp",
+        href: "https://www.cnblogs.com/",
+      },
+      {
+        title: "站长素材",
+        pic: "爱奇艺.webp",
+        href: "https://sc.chinaz.com/",
+      },
+      {
+        title: "阿里巴巴矢量图标库",
+        pic: "阿里巴巴矢量图标库.png",
+        href: "https://www.iconfont.cn/",
+      },
+      {
+        title: "icomoon",
+        pic: "icomoon.png",
+        href: "https://icomoon.io/app/#/select",
+      },
+      {
+        title: "css颜色表",
+        pic: "css颜色表.png",
+        href: "https://flatuicolors.com/",
+      },
+      {
+        title: "菜鸟教程",
+        pic: "爱奇艺.webp",
+        href: "https://www.runoob.com/",
+      },
+      {
+        title: "黑马前端",
+        pic: "黑马前端.png",
+        href: "https://space.bilibili.com/415434293?spm_id_from=333.788.b_765f7570696e666f.1",
+      },
+      {
+        title: "css渐变色",
+        pic: "爱奇艺.webp",
+        href: "http://color.oulu.me/",
+      },
+      {
+        title: "豌豆荚",
+        pic: "豌豆荚.webp",
+        href: "https://www.wandoujia.com/",
+      },
+      {
+        title: "Iconscout",
+        pic: "Iconscout.png",
+        href: "https://iconscout.com/unicons/explore/line",
+      },
+      {
+        title: "YApi",
+        pic: "YApi.png",
+        href: "https://mock.duyiedu.com/project/72/interface/api/135",
+      },
+      {
+        title: "掘金",
+        pic: "掘金.png",
+        href: "https://juejin.cn/",
+      },
+      {
+        title: "Mdn",
+        pic: "Mdn.png",
+        href: "https://developer.mozilla.org./zh-CN/",
+      },
+      {
+        title: "牛客网",
+        pic: "牛客.webp",
+        href: "https://www.nowcoder.com/",
+      },
+  ],
+  bitemData: [
+    {
+      title: "响应式社交网",
+      pic: "item.jpg",
+      href: "file:///D:/%E5%89%8D%E7%AB%AF%E5%AD%A6%E4%B9%A0/%E7%9B%B4%E6%92%AD%E5%BA%94%E7%94%A8%E5%AE%9E%E6%88%98/html%20css%20%E5%93%8D%E5%BA%94%E5%BC%8F%E7%A4%BE%E4%BA%A4%E7%BD%91%E7%AB%99/index.html",
+    },
+    {
+        title: "bookmark mannager网页版",
+        pic: "item.jpg",
+        href: "file:///D:/%E5%A4%A7%E5%AD%A6%E5%AD%A6%E4%B9%A0/html5+css3/%E9%A1%B9%E7%9B%AE/%E4%B9%A6%E7%AD%BE%E7%AE%A1%E7%90%86/index.html",
+      },
+      {
+        title: "future触屏版",
+        pic: "item.jpg",
+        href: "file:///D:/%E5%A4%A7%E5%AD%A6%E5%AD%A6%E4%B9%A0/html5+css3/%E9%A1%B9%E7%9B%AE/future%E7%A7%BB%E5%8A%A8%E7%AB%AF%E7%BD%91%E7%AB%99/common.html",
+      },
+      {
+        title: "Schedule",
+        pic: "item.jpg",
+        href: "schedule.html",
+      },
+      {
+        title: "抓小羊",
+        pic: "item.jpg",
+        href: "file:///D:/%E5%89%8D%E7%AB%AF%E5%AD%A6%E4%B9%A0/%E7%9B%B4%E6%92%AD%E5%BA%94%E7%94%A8%E5%AE%9E%E6%88%98/sheepCatch/index.html",
+      },
+      {
+        title: "Evolution",
+        pic: "item.jpg",
+        href: "file:///D:/%E5%A4%A7%E5%AD%A6%E5%AD%A6%E4%B9%A0/html5+css3/%E9%A1%B9%E7%9B%AE/website/Evolution/index.html",
+      },
+  ],
+};
---
+++ js/script.js
@@ -0,0 +1,75 @@
+function my$(id) {
+  return document.getElementById(id);
+}
+var bookmarks = ["bvedio", "bstudy", "bentertainment", "bcode", "bitem"];
+var navmarks = ["vedio", "study", "play", "code", "item"];
+(function () {
+  for (let i = 0; i < navmarks.length; i++) {
+    my$(navmarks[i]).onclick = function () {
+      for (let j = 0; j < navmarks.length; j++) {
+        my$(bookmarks[j]).style.display = "none";
+        my$(navmarks[j]).style.color = "black";
+      }
+      my$(bookmarks[i]).style.display = "block";
+      my$(navmarks[i]).style.color = "red";
+    };
+  }
+
+  let plane = document.getElementById("plane");
+  let deg = 0,
+    ex = 0,
+    ey = 0,
+    vx = 0,
+    vy = 0,
+    count = 0;
+  window.addEventListener("mousemove", (e) => {
+    ex = e.x - plane.offsetLeft - plane.clientWidth / 2;
+    ey = e.y - plane.offsetTop - plane.clientHeight / 2;
+    deg = (360 * Math.atan(ey / ex)) / (2 * Math.PI) + 45;
+    if (ex < 0) {
+      deg += 180;
+    }
+    count = 0;
+  });
+  function draw() {
+    plane.style.transform = "rotate(" + deg + "deg)";
+    if (count < 100) {
+      vx += ex / 100;
+      vy += ey / 100;
+    }
+    plane.style.left = vx + "px";
+    plane.style.top = vy + "px";
+    count++;
+  }
+  setInterval(draw, 1);
+})();
+// 加载数据
+(function () {
+  function loadData() {
+    for (let i = 0; i < bookmarks.length; i++) {
+      var mark = document.querySelector("#" + bookmarks[i]);
+      var ul = document.createElement("ul");
+      var dataname = bookmarks[i]+'Data';
+      var dataItem = data[dataname];
+      for (let j = 0; j < data[dataname].length; j++) {
+        var li = document.createElement("li");
+        var a = document.createElement("a");
+        var div = document.createElement("div");
+        var img = document.createElement("img");
+        var span = document.createElement("span");
+        a.href = dataItem[j].href;
+        a.target = '_blank'
+        div.className = 'ico';
+        img.src = './ico/'+dataItem[j].pic;
+        span.innerHTML = dataItem[j].title;
+        div.appendChild(img);
+        a.appendChild(div);
+        a.appendChild(span);
+        li.appendChild(a);
+        ul.appendChild(li);
+      }
+      mark.appendChild(ul);
+    }
+  }
+  loadData();
+})();
